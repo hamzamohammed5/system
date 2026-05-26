16f15aa245b37b29597c0a2997248d2d7169312f
hamza mohammed <107774485+hamzamohammed5@users.noreply.github.com>
2026-05-26 14:02:27 +0300

    .

--- ui/widgets/base/crud_form.py
+++ ui/widgets/base/crud_form.py
@@ -7,7 +7,8 @@ from PyQt5.QtCore    import pyqtSignal, Qt
 
 from ui.app_settings import _C
 from ..panels.form_parts         import FormGroup
-from ..components.label          import ModeLabelfrom ..components.notification   import NotificationBar
+from ..components.label        import ModeLabel
+from ..components.notification import NotificationBar
 from ..components.button         import make_btn
 from ..mixins.edit               import EditModeMixin
 from ..mixins.validate           import FormValidationMixin
